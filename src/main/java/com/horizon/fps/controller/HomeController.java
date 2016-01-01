f733fc19a35325ad43f529236d200134e12bea98
horizon <rdvn.tpl@gmail.com>
2016-01-01 23:57:24 +0200

    hiii12

--- src/main/java/com/horizon/fps/controller/HomeController.java
+++ src/main/java/com/horizon/fps/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.horizon.fps.controller;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +25,20 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home() {
 
+		Socket conn;
+		try {
+			conn = new Socket("4865f12a.carbon.hostedgraphite.com", 2003);
+			DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+			dos.writeBytes("9b917ef5-4391-4b5f-95a3-a1bf8f2c33f3.test.testing 1.2\n");
+			conn.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		ModelAndView home = new ModelAndView("home");
 		home.addObject("products",pService.selectAllFromDB());
 		home.addObject("user",new User());
--- src/main/java/com/horizon/fps/controller/ManagerController.java
+++ src/main/java/com/horizon/fps/controller/ManagerController.java
@@ -1,5 +1,10 @@
 package com.horizon.fps.controller;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +23,20 @@ public class ManagerController {
 	
 	@RequestMapping(value = "/")
 	public ModelAndView index() {
-
+		
+		Socket conn;
+		try {
+			conn = new Socket("4865f12a.carbon.hostedgraphite.com", 2003);
+			DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+			dos.writeBytes("9b917ef5-4391-4b5f-95a3-a1bf8f2c33f3.test.testing 1.2\n");
+			conn.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return new ModelAndView("manager/index");
 	}
